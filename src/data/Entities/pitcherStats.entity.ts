ed7d56456cde495476afde61c44afe4ce12db867
Adam C. MacKinnon <adamcmackdev@gmail.com>
2025-08-28 22:27:05 -0400

    updated entities, fixed redundancies

--- src/data/Entities/pitcherStats.entity.ts
+++ src/data/Entities/pitcherStats.entity.ts
@@ -16,32 +16,45 @@ export class PitcherStats {
   games: number;
   @Column()
   games_started: number;
-  @Column()
+  @Column('numeric', { precision: 5, scale: 1 })
   innings_pitched: number;
   @Column('numeric', { precision: 3, scale: 2 })
+  @Column('numeric', { precision: 5, scale: 2 })
   k_per_9: string;
-  @Column('numeric', { precision: 3, scale: 2 })
+
+  @Column('numeric', { precision: 5, scale: 2 })
   bb_per_9: string;
-  @Column('numeric', { precision: 3, scale: 2 })
+
+  @Column('numeric', { precision: 5, scale: 2 })
   hr_per_9: string;
-  @Column('numeric', { precision: 3, scale: 3 })
+
+  @Column('numeric', { precision: 5, scale: 3 })
   babip_against: string;
-  @Column('numeric', { precision: 3, scale: 2 })
+
+  @Column('numeric', { precision: 5, scale: 2 })
   lob_pct: string;
-  @Column('numeric', { precision: 3, scale: 2 })
+
+  @Column('numeric', { precision: 5, scale: 2 })
   gb_pct: string;
-  @Column('numeric', { precision: 3, scale: 2 })
+
+  @Column('numeric', { precision: 5, scale: 2 })
   hr_fb: string;
-  @Column('numeric', { precision: 3, scale: 1 })
+
+  @Column('numeric', { precision: 5, scale: 1, nullable: true })
   velo_fb: string;
-  @Column('numeric', { precision: 3, scale: 2 })
+
+  @Column('numeric', { precision: 5, scale: 2 })
   era: string;
-  @Column('numeric', { precision: 3, scale: 2 })
+
+  @Column('numeric', { precision: 5, scale: 2 })
   x_era: string;
-  @Column('numeric', { precision: 3, scale: 2 })
+
+  @Column('numeric', { precision: 5, scale: 2 })
   fip: string;
-  @Column('numeric', { precision: 3, scale: 2 })
+
+  @Column('numeric', { precision: 5, scale: 2 })
   x_fip: string;
-  @Column('numeric', { precision: 3, scale: 2 })
+
+  @Column('numeric', { precision: 5, scale: 2 })
   f_war: string;
 }
